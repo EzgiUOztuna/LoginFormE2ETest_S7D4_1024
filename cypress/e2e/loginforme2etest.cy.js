156388b9d9e441c762c9dbc0c5a82b85a814826b
Ezgi Uygun Öztuna <ezgiuygn@gmail.com>
2024-11-28 17:24:08 +0300

    cypress_tests

--- cypress/e2e/loginforme2etest.cy.js
+++ cypress/e2e/loginforme2etest.cy.js
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:5173/')
-  })
+describe('Login tests', () => {
+  //Arrange
+  it('should add some text to the name input', () => {
+      cy.visit('http://localhost:5173/')
+
+      //Act
+      cy.get('[data-cy="email-input"]').type("erdem.guntay@wit.com.tr")
+      cy.get('[data-cy="password-input"]').type("9fxIH0GXesEwH_I")
+      cy.get('[data-cy="terms-input"]').click()
+      cy.get('[data-cy="submit-button"]').click()
+      
+      
+
+      //Assert
+      
+
+
+
+
+    })
+
+
+
+
 }) 
